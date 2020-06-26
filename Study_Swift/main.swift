0c41dbe7412a76ec71a93ce5a73d1f608c204e73
Harry-Bro <likeharrybro@gmail.com>
2020-06-27 03:39:10 +0900

    start swift

--- Study_Swift/main.swift
+++ Study_Swift/main.swift
@@ -1,12 +1,10 @@
-//
-//  main.swift
-//  Study_Swift
-//
-//  Created by HarryBro on 2020/06/27.
-//  Copyright © 2020 HarryBro. All rights reserved.
-//
 
 import Foundation
 
 print("Hello, World!")
 
+// 변수 타입을 지정을 안해도 됨.
+// 하지만 변수에 값을 넣지 않고 선언만 먼저 할 경우에는 변수타입을 반드시 지정해줘야함
+
+var str = "hi swift"
+print(str)
